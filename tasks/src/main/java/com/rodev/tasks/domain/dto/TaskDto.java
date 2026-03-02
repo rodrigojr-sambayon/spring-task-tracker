727db809a6cfd93385a18a2cf7138b46c3906b59
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-03-03 05:19:21 +0800

    Add TaskDto

---
+++ tasks/src/main/java/com/rodev/tasks/domain/dto/TaskDto.java
@@ -0,0 +1,18 @@
+package com.rodev.tasks.domain.dto;
+
+import com.rodev.tasks.domain.entities.TaskPriority;
+import com.rodev.tasks.domain.entities.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TaskDto(
+        UUID id,
+        String title,
+        String description,
+        LocalDateTime dueDate,
+        TaskPriority priority,
+        TaskStatus status
+) {
+
+}
