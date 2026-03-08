c443f6adecef1e8b838ce705dd4ffc6c2a8a6e95
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-03-08 22:23:44 +0800

    Addt TaskListController

---
+++ tasks/src/main/java/com/rodev/tasks/controllers/TaskListController.java
@@ -0,0 +1,33 @@
+package com.rodev.tasks.controllers;
+
+import com.rodev.tasks.domain.dto.TaskListDto;
+import com.rodev.tasks.domain.entities.TaskList;
+import com.rodev.tasks.mappers.TaskListMapper;
+import com.rodev.tasks.services.TaskListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+}
